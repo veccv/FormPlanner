ff2fbc52ec6305ac2f90660da6a96a854fe082a9
Jacek Dalmer <jacek.dalmer@pbs.edu.pl>
2022-08-17 12:56:27 +0200

    Initial commit

---
+++ src/main/java/com/github/veccv/formplanner/FormPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.veccv.formplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/veccv/formplanner/FormPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.veccv.formplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
